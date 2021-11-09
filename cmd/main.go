d10658430441180b71d0f9bc945cf4404de93822
Adrian <alic.adrian.berlin@gmail.com>
2021-11-09 20:17:27 +0100

    Init Commit

---
+++ cmd/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"github.com/m2q/siam-cs/csgo"
+)
+
+func main() {
+
+	hltv := csgo.HLTV{}
+
+	err := hltv.Fetch()
+	if err != nil {
+		fmt.Errorf(err.Error())
+	}
+	_, _ = hltv.GetFutureMatches()
+	_, _ = hltv.GetPastMatches()
+
+}
