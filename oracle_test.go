854023552d34ed39eff8a87319abb4e44f72c22d
Adrian <alic.adrian.berlin@gmail.com>
2021-11-11 16:28:57 +0100

    Move data conversion and fetch into separate file

--- oracle_test.go
+++ oracle_test.go
@@ -3,14 +3,19 @@ package csgo
 import (
 	siam "github.com/m2q/algo-siam"
 	"github.com/m2q/algo-siam/client"
+	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
 // setupOracleMockedAPI creates an Oracle instance, initialized with a
 // AlgorandBuffer (client.AlgorandMock) and StubAPI.
 func setupOracleMockedAPI() (*Oracle, *siam.AlgorandBuffer, *StubAPI) {
 	c := client.CreateAlgorandClientMock("", "")
-	buffer, _ := siam.CreateAlgorandBuffer(c, "")
+	buffer, err := siam.CreateAlgorandBuffer(c, client.GeneratePrivateKey64())
+	if err != nil {
+		panic(err)
+	}
 	api := &StubAPI{}
 	cfg := &OracleConfig{
 		PrimaryAPI:      nil,
@@ -20,13 +25,9 @@ func setupOracleMockedAPI() (*Oracle, *siam.AlgorandBuffer, *StubAPI) {
 	return o, buffer, api
 }
 
-func getTestingData(path string) []Match {
-	return nil
-}
-
 // Tests if the Oracle writes the data provided by the API stub to the AlgorandBuffer
 func TestOracle_SimpleSmallData(t *testing.T) {
-	oracle, _, stub := setupOracleMockedAPI()
+	oracle, buffer, stub := setupOracleMockedAPI()
 	oracle.Serve()
 
 	// Set match data to stub
@@ -34,8 +35,8 @@ func TestOracle_SimpleSmallData(t *testing.T) {
 	stub.SetMatches(matchData, 5)
 
 	// Check if data is being written to the AlgorandBuffer
-	// contains, _ := buffer.ContainsWithin(MatchesToMap(matchData), time.Second)
-	// assert.True(t, contains)
+	contains := buffer.ContainsWithin(CreateWinnerMap(matchData), time.Second)
+	assert.True(t, contains)
 
 	// Stop oracle
 	oracle.Stop()
---
+++ processing.go
@@ -0,0 +1,11 @@
+package csgo
+
+// CreateWinnerMap converts a slice []Match into a map, where the key is the match ID,
+// and the value is the Match winner. If there is no winner yet, the value will be empty.
+func CreateWinnerMap(m []Match) map[string]string {
+	return nil
+}
+
+func getTestingData(path string) []Match {
+	return nil
+}
